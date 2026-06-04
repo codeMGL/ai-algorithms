b5c2dc2a206a39d43b67458c96503f4d3f69ad1c
codeMGL <marco.gajlop@gmail.com>
2026-06-04 11:35:56 +0200

    Implemented test for DFS

--- tests/search_algorithms/test_bfs_dfs.py
+++ tests/search_algorithms/test_bfs_dfs.py
@@ -2,12 +2,13 @@ import networkx as nx
 import pytest
 import random
 
-from search_algorithms import BFS
+from search_algorithms import BFS, DFS
 from utils import SearchNode, Visualizer
 
 random.seed(42)
 
 W, H = 1200, 700
+test_values = [1, 10, 100, 1000, 5_000, 10_000, 15_000]
 
 def random_tree_generator(n):
     start = SearchNode("0", root=True, W=W)
@@ -19,11 +20,10 @@ def random_tree_generator(n):
     if n == 1:
         G.add_edge(start.id, start.id)
         return start, start, G
-    
-    
+
     while i < n:
         node = start
-        # There's a 50% chance to choose the children
+        # There's a 70% chance to choose the children
         while random.random() < 0.7 + 0.08 * node.children_count:
             if node.children_count != 0:
                 node = random.choice(node.children)
@@ -38,24 +38,41 @@ def random_tree_generator(n):
             # Adding an edge to the NetworkX Graph
             G.add_edge(new_node.id, node.id)
             i += 1
-            
+
     goal = new_node
     goal.color = (50, 80, 100)
     return start, goal, G
 
 
-@pytest.mark.parametrize("n", [1, 10, 100, 1000, 5_000, 10_000, 20_000])
-def test_sort(n):
+@pytest.mark.parametrize("n", test_values)
+def test_bfs(n):
     start, goal, G = random_tree_generator(n)
     bfs = BFS(start, goal)
     bfs.run()
-    
+
     # vis = Visualizer(W, H, root=start)
     # vis.run()
 
     path = nx.shortest_path(G, start.id, goal.id)
-    
-    print(n, bfs.get_ids_path())
-    # print(path)
-    
-    assert bfs.get_ids_path() == path
+
+    bfs_path = [node.id for node in bfs.path if node.id]
+    print(n, bfs_path)
+
+    assert bfs_path == path
+
+
+@pytest.mark.parametrize("n", test_values)
+def test_dfs(n):
+    start, goal, G = random_tree_generator(n)
+    dfs = DFS(start, goal)
+    dfs.run()
+
+    # vis = Visualizer(W, H, root=start)
+    # vis.run()
+
+    path = nx.shortest_path(G, start.id, goal.id)
+
+    dfs_path = [node.id for node in dfs.path if node.id]
+    print(n, dfs_path)
+
+    assert dfs_path == path
